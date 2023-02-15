56d3ccbf473a13db28720e8a7fdfd1d21d7f7354
Richard <richardhayes777@gmail.com>
2023-02-15 21:18:27 +0000

    cache ip

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import time
+from pathlib import Path
 
 import discoverhue
 import phue
 
 
-def get_bridge():
+def find_ip():
     bridges = discoverhue.find_bridges()
     url = list(bridges.values())[0]
-    ip = url.split(":")[1].strip("/")
+    return url.split(":")[1].strip("/")
 
+
+def get_ip():
+    ip_file_path = Path.home() / ".ip.txt"
+    if ip_file_path.exists():
+        return ip_file_path.read_text()
+    else:
+        ip = find_ip()
+        ip_file_path.write_text(ip)
+        return ip
+
+
+def get_bridge():
     while True:
         try:
-            return phue.Bridge(ip)
+            return phue.Bridge(get_ip())
         except phue.PhueRegistrationException:
             print("Press the button on the bridge")
             time.sleep(2)
