839fec8e9748e048030a9998eb8d73257f1a3051
Richard <richardhayes777@gmail.com>
2023-02-15 21:30:50 +0000

    name of color to hue

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
 import time
 from pathlib import Path
 
 import discoverhue
+import openai
 import phue
+
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 def find_ip():
@@ -29,9 +34,33 @@ def get_bridge():
             print("Press the button on the bridge")
             time.sleep(2)
 
+
+# Set the model and prompt
+model_engine = "text-davinci-003"
+prompt = "Give me the value of hue from 0 to 65535 which corresponds to turquoise. Answer only with the number."
+
+# Set the maximum number of tokens to generate in the response
+max_tokens = 1024
+
+# Generate a response
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=max_tokens,
+    temperature=0.5,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+response = completion.choices[0].text
+# Print the response
+print(response)
+
+
 bridge = get_bridge()
 
-bridge.lights[0].hue = 21845
+bridge.lights[0].hue = int(response)
 
 # for light in bridge.lights:
 #     print(light.name)
